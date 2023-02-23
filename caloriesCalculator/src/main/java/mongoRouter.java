be11a2c65bd2b77886cfb6d37a3bc8e81fea3cf5
pochun-li <pochun.li.847@gmail.com>
2023-02-22 21:30:57 -0800

    Initial Commit: maven project setup and CRUD function

---
+++ caloriesCalculator/src/main/java/mongoRouter.java
@@ -0,0 +1,9 @@
+package main.java;
+import mongodb.client.MongoClient;
+import mongodb.client.MongoClients;
+import mongodb.client.MongoCollection;
+import mongodb.client.MongoDatabase;
+import bson.Document;
+
+public class mongoRouter {
+}
---
+++ caloriesCalculator/src/main/java/user/User.java
@@ -0,0 +1,16 @@
+package main.java.user;
+
+import java.time.LocalDate;
+
+public class User {
+
+    int userId;
+    String email;
+    String passwords;
+    String firstName;
+    String lastName;
+    String gender;
+    LocalDate date;
+
+
+}
